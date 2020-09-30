1dd6da39b36cf0c4eeaf3e8b65fb20a7361558c9
Antons Senčevs <st60616@TTI.LV>
2020-09-30 11:41:21 +0300

    fixes

--- GraphLab1_2/Source.cpp
+++ GraphLab1_2/Source.cpp
@@ -5,10 +5,10 @@
 #include <string>
 #include <list>
 #include <iterator> 
-
+#include <vector>
 
 #if defined(linux) || defined(_WIN32)
-#include<GL/glut.h>/*ДляLinux и Windows*/
+#include"C:\Program Files (x86)\Microsoft Visual Studio 11.0\GL\glut.h"/*ДляLinux и Windows*/
 #else
 #include<GLUT/GLUT.h>/*Для Mac OS*/
 #endif
@@ -21,8 +21,8 @@ struct Point {
 	float y;
 };
 
-
-list <Point> points;
+const int MINX = 0, MINY = 0, MAXX = 20, MAXY = 20;
+vector <Point> points;
 list <int> code;
 Point currPoint;
 
@@ -45,15 +45,16 @@ void reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(-10, 10, -10, 10);
+	gluOrtho2D(MINX, MAXX, MINY, MAXY);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
 
-void readFromFile()
+void readFromFile(string path)
 {
-	fstream f("points.txt", ios::in);
+	fstream f(path, ios::in);
+	cout << path << endl;
 	if (f.is_open()) {
 		int pointNumber;
 		Point p;
@@ -73,51 +74,65 @@ void readFromFile()
 			code.push_back(m);
 			cout << m << endl;
 		}
+		cout << endl;
 	}
 	f.close();
 }
 
 
-void moveTo(int mto) {
+void moveTo(int mto, float shiftX, float shiftY) {
 	mto = abs(mto) - 1;
-	list <Point> ::iterator it = points.begin();
-	for (int i = 0; i < mto; i++) {
-		it++;
-	}
-	currPoint.x = (*it).x;
-	currPoint.y = (*it).y;
+	currPoint.x = points[mto].x + shiftX;
+	currPoint.y = points[mto].y + shiftY;
 
 }
 
-void drawTo(int dto) {
-	dto = abs(dto) - 1;
-	list <Point> ::iterator it = points.begin();
-	for (int i = 0; i < dto; i++) {
-		it++;
-	}
-	Point p = (*it);
+void drawTo(int dto, float shiftX, float shiftY) {
+	dto = dto - 1;
+  //  points[dto];
+	Point p = points[dto];
 	glColor3d(1, 1, 1);
 	glBegin(GL_LINES);
 	glVertex2i(currPoint.x, currPoint.y);
-	glVertex2i(p.x, p.y);
+	glVertex2i(p.x+shiftX, p.y+shiftY);
 
 	glEnd();
-	currPoint = p;
+	currPoint.x = p.x + shiftX;
+	currPoint.y = p.y + shiftY;
+}
+
+void drawOrMove(float shiftX, float shiftY) {
+
+	for (auto it = code.begin(); it != code.end(); it++) {
+		if ((*it) < 0) {
+			moveTo((*it), shiftX, shiftY);
+		}
+		else {
+			drawTo((*it), shiftX,shiftY);
+		}
+	}
+
+	code.clear();
+	points.clear();
 }
 
 void display() {
-	readFromFile();
 
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	for (auto it = code.begin(); it != code.end(); it++) {
-		if ((*it) < 0) {
-			moveTo((*it));
-		}
-		else {
-			drawTo((*it));
-		}
-	}
+	float shiftX = 5;
+	float shiftY = 5;
+	readFromFile("points.txt");
+	drawOrMove(shiftX,shiftY);
+	
+
+	readFromFile("a.txt");
+	drawOrMove(0,0);
+
+
+	readFromFile("s.txt");
+	drawOrMove(0,0);
+	
 	glutSwapBuffers();
 }
